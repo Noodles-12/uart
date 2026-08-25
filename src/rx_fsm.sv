a6f51f26600a91eecbef8f0f8f1c3c9941dce83b
Noodles-12 <mahiyan21@gmail.com>
2026-08-25 15:03:52 -0500

    rx progress

--- src/rx_fsm.sv
+++ src/rx_fsm.sv
@@ -9,9 +9,21 @@ module rx_fsm(
     output logic [7:0] rx_data,
     output logic rx_ready
 );
+    typedef enum logic [1:0] {
+        IDLE = 2'b00,
+        START_BIT = 2'b01,
+        DATA_BITS = 2'b10,
+        STOP_BIT = 2'b11
+    } rx_state;
+
+    rx_state current_state;
 
     always_ff @ (posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            current_state <= IDLE;
+        end else begin
 
+        end
     end
 
 endmodule
